7bcfacb3943b19f6b37ebe2edf6dcc5ad541551b
MinJoon Joo <tuk34896@temple.edu>
2024-02-08 12:03:02 -0500

    updated main and textsizeadapter
    created getdropdownview, and changed the line for mainactivity to use textsizeadapter we created

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -3,23 +3,46 @@ package edu.temple.inclassuiacvitivity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.content.Context
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter (private val context: Context, private val textSize: Array<Int>) :BaseAdapter(){
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+
+        return textSize.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+
+        return textSize[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        return (getDropDownView(position, convertView, parent) as TextView).apply{
+            textSize = 30.0f
+        }
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+        val tempTextView : TextView
+        if (convertView != null){
+            tempTextView = convertView as TextView
+        }else{
+            tempTextView = TextView(context)
+            tempTextView.setPadding(5,10,0,10)
+        }
+
+        tempTextView.text = textSize[position].toString()
+        tempTextView.textSize = textSize[position].toFloat()
+
+        return tempTextView
     }
 
 }
